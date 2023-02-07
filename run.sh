300ef9a7fe7fdb76c66cd36ed3d4aa8cf7f649af
ModoStudio <marcin.grabowski@modostudio.pl>
2023-02-07 11:11:59 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 docker pull modostudio/binance-copy-bot
-docker-compose stop
-docker-compose rm -f
-docker-compose build
-docker-compose up -d
+docker compose stop
+docker compose rm -f
+docker compose build
+docker compose up -d
 
 exec "$@"
